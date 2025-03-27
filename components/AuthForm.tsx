2a6841cb5e38bbef0407795398addab7440e1b9e
Wilson Chan <wilsonchan54@gmail.com>
2025-03-27 16:04:20 -0400

    npx install shadcn-ui input / updated next.config.ts and globals.css

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,5 +1,7 @@
 "use client"
+
 import React from 'react'
+import Image from 'next/image'
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -14,6 +16,7 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
+import { Input } from './ui/input'
 
 
 const formSchema = z.object({
@@ -22,6 +25,8 @@ const formSchema = z.object({
 
 
 const AuthForm = () => {
+  const logo=`https://images.stockcake.com/public/7/d/e/7de5e9f4-7e5a-4c29-b504-90cb82cfb0f5_large/mystical-chain-magic-stockcake.jpg`
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -37,9 +42,22 @@ const AuthForm = () => {
     console.log(values)
   }
   return (
-    <div>
+    <div className='card_border lg:min-w-[600px]'>
+      <div className="flex flex-col gap-8 card py-3 px-10">
+        <div className="flex flex-row gap-2 justify-center">
+          <Image 
+            src={logo}
+            alt='logo'
+            height={40}
+            width={100}
+            className='rotate-90 rounded-md'
+          />
+          <h2 className="ml-10 text-indigo-500 bg-indigo-900 p-1.5 rounded-md">Rope</h2>
+        </div>
+      </div>
+
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 bg-blue-900">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 p-4 mt-2 bg-blue-900">
           <FormField
             control={form.control}
             name="username"
@@ -47,7 +65,7 @@ const AuthForm = () => {
               <FormItem>
                 <FormLabel>Username</FormLabel>
                 <FormControl>
-                  <div className="bg-amber-600 flex justify-center">NULL</div>
+                  <Input placeholder="shadcn" {...field} className='border border-amber-500' />
                 </FormControl>
                 <FormDescription>
                   This is your public display name.
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns:[
+      {
+        protocol: 'https',
+        hostname: '*',
+      },
+      // {
+      //   protocol: 'https',
+      //   hostname: '',
+      // },
+      // {
+      //   protocol: 'https',
+      //   hostname: '',
+      //   port: '',
+      //   pathname: '',
+      // },
+    ]
+  }
 };
 
 export default nextConfig;
