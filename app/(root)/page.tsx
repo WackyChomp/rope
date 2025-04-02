4f68d9fffe5169628c34ca6b8536bfb74f64374c
Wilson Chan <wilsonchan54@gmail.com>
2025-04-01 20:38:32 -0400

    Created placeholder outline in root home page.tsx / added styling in globals.css

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,48 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+import { Button } from '@/components/ui/button'
 
 const page = () => {
+  const img = `https://media.tenor.com/ZFoOuUSqPBEAAAAi/stalzone-meme.gif`;
+
   return (
-    <div className="bg-red-500">
-      Home Page
-    </div>
+    <>
+      <section className='card_cta'>
+        <div className="bg-red-500 flex flex-col gap-6 max-w-lg">
+          <h2 className='capitalize text-5xl'>Perform at your peak off the cuff through rigorous practice & feedback</h2>
+          <p className="text-lg">Refine your expertise through trials and tribulations!</p>
+
+          <Button asChild className='btn_primary max-sm:w-full' >
+            <Link href='/interview'>Start an Interview</Link>
+          </Button>
+        </div>
+
+        <Image 
+          src={img} 
+          alt='img' 
+          width={400} 
+          height={400}
+          className='max-sm:hidden rounded-xl border-6 border-amber-900'
+        />
+      </section>
+
+      <section className="flex flex-col gap-7 mt-8">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">You haven&apos;t done any interviews</div>
+      </section>
+
+      <section className="flex flex-col gap-7 mt-8">
+        <div className="interviews-section">
+          There are no interviews available {`:^)`}
+        </div>
+      </section>
+
+      {/* <section className="flex flex-col gap-7 mt-8">
+
+      </section> */}
+    </>
   )
 }
 
