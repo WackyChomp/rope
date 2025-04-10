e82c4eedef0904c51e0f6754bb1a5b9f06e6f089
Wilson Chan <wilsonchan54@gmail.com>
2025-04-10 15:32:04 -0400

    Created server action for sign-in and cookie in auth.actions.ts / called sign-in server action in AuthForm component

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -13,9 +13,11 @@ import FormField from './FormField'
 import { toast } from 'sonner'
 import { useRouter } from 'next/navigation'
 
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '@/firebase/client'
 import { signUp } from '@/lib/actions/auth.action'
+import { signIn } from '@/lib/actions/auth.action'
+
 
 const authFormSchema = (type : FormType) => {
   return z.object({
@@ -65,6 +67,20 @@ const AuthForm = ({ type }: { type: FormType }) => {
         toast.success('Account created successfully. Go sign in!')
         router.push('/sign-in')
       } else{
+        const { email, password } =  values;
+
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+        const idToken = await userCredential.user.getIdToken();
+
+        if(!idToken){
+          toast.error('Sign in failed');
+          return
+        }
+
+        await signIn({
+          email, idToken
+        })
         console.log('You have SIGN IN', values)
         toast.success('Signed in successfully!')
         router.push('/')
@@ -75,6 +91,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
     }
   }
 
+  
   const isSignIn = type === 'sign-in';
 
   return (
